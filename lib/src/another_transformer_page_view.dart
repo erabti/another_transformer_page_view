b4ad54f318aab91bc7b14b3f1659bcd883c51af9
Ahmed Erabti <a.erabti@email.ly>
2022-05-15 09:36:04 +0200

    migrate to flutter 3

--- lib/src/another_transformer_page_view.dart
+++ lib/src/another_transformer_page_view.dart
@@ -516,7 +516,7 @@ class _TransformerPageViewState extends State<TransformerPageView> {
       }
     }
     if (_transformer != null) {
-      WidgetsBinding.instance!.addPostFrameCallback(_onGetSize);
+      WidgetsBinding.instance.addPostFrameCallback(_onGetSize);
     }
 
     if (_controller != getNotifier()) {
@@ -534,7 +534,7 @@ class _TransformerPageViewState extends State<TransformerPageView> {
   @override
   void didChangeDependencies() {
     if (_transformer != null) {
-      WidgetsBinding.instance!.addPostFrameCallback(_onGetSize);
+      WidgetsBinding.instance.addPostFrameCallback(_onGetSize);
     }
     super.didChangeDependencies();
   }
